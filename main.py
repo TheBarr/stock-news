9d59bb3fcee26bd88c4a22addbe6c1d82678266a
bartek <bartex600700@gmail.com>
2023-07-20 16:50:11 +0200

    gitignore

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import datetime as dt
 import smtplib
 from dotenv import load_dotenv
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 load_dotenv()
 
@@ -28,44 +29,43 @@ stock_data_day_before_yesterday = float(
 
 percentage = round(100 - (stock_data_day_before_yesterday / stock_data_yesterday) * 100, 2)
 
-if percentage >= 0.1 or percentage <= -0.1:
+if percentage >= 5 or percentage <= -5:
     news_parameters = {
         "apiKey": os.getenv("NEWS_KEY"),
         "q": COMPANY_NAME,
-        "searchIn": "title","description"
-        "from": date_day_before_yesterday,
+        "searchIn": "title", "description"
+                             "from": date_day_before_yesterday,
         "to": date_yesterday,
         "sortBy": "relevancy",
-        "pageSize":3,
+        "pageSize": 3,
     }
 
     news_response = requests.get("https://newsapi.org/v2/everything", params=news_parameters)
     news_response.raise_for_status()
 
-    articles = ""
-    for i in range(3):
-        articles += "Title: " + (news_response.json()["articles"][i]["title"])
-        articles += ("\n")
-        articles += "Description: " + (news_response.json()["articles"][i]["description"])
-        articles += ("\n\n")
-
-    if percentage >= 0.1:
+    if percentage >= 5:
         title = f"{COMPANY_NAME} Stock And News - {STOCK} 🔺{percentage}%"
-    elif percentage <= -0.1:
+    elif percentage <= -5:
         title = f"{COMPANY_NAME} Stock And News - {STOCK} 🔻{percentage}%"
 
     sender = os.getenv("FROM_EMAIL")
     receivers = os.getenv("TO_EMAIL")
 
-    msg = EmailMessage()
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = title
     msg['From'] = sender
     msg['To'] = receivers
-    msg['Subject'] = title
-    msg.set_content(articles)
+
+    articles = """<html><head></head><body>"""
+    for i in range(3):
+        articles += "<b>" + news_response.json()["articles"][i]["title"] + "</b>" + "<br>"
+        articles += news_response.json()["articles"][i]["description"] + "<br>" + "<br>"
+    articles += """</body> </html>"""
+    text = MIMEText(articles, "html")
+    msg.attach(text)
 
     with smtplib.SMTP("smtp.gmail.com") as conn:
         conn.starttls()
         conn.login(user=sender, password=os.getenv("FROM_EMAIL_PASSWORD"))
-        conn.send_message(msg)
+        conn.sendmail(sender, receivers, msg.as_string())
         print("Email sent")
-
